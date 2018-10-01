2eacf71790ab49a29e6c558d662cd188bc0abfc5
Patrick Robins <S00091240@mail.itsligo.ie>
2018-10-01 11:49:47 +0100

    Filter Working

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { IProduct } from './product';
 
 @Component({
   selector: 'app-product-list',
@@ -8,8 +9,20 @@ import { Component, OnInit } from '@angular/core';
 export class ProductListComponent {
 
   pageTitle: string = "Tom's Products";
+  imageWidth: number = 50;
+  imageMargin: number = 2;
+  showImage: boolean = false;
+  _listFilter: string;
+  get listFilter(): string {
+      return this._listFilter;
+  }
+  set listFilter(value: string){
+      this._listFilter = value;
+      this.filteredProducts = this.listFilter ? this.performFilter(this.listFilter) : this.products;
+  }   
 
-  products: any[] = [
+  filteredProducts: IProduct[];
+  products: IProduct[] = [
     {
         "productId": 1,
         "productName": "Leaf Rake",
@@ -60,5 +73,17 @@ export class ProductListComponent {
         "starRating": 4.6,
         "imageUrl": "http://openclipart.org/image/300px/svg_to_png/120337/xbox-controller_01.png"
     }
-]
+];
+
+constructor(){
+    this.filteredProducts = this.products;
+}
+performFilter(filterBy: string): IProduct[]{
+    filterBy = filterBy.toLocaleLowerCase();
+    return this.products.filter((product: IProduct) =>
+    product.productName.toLocaleLowerCase().indexOf(filterBy) !== -1);
+}
+toggleImage():void{
+    this.showImage = !this.showImage;
+}
 }
